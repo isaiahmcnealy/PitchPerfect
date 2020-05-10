d1b2ccac6af3ba44b4b3f79469240c7c2c80ac4a
Isaiah <i-mcnealy@outlook.com>
2020-05-09 23:51:39 -0400

    Clean up

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -18,6 +18,10 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var stopRecordingButton: UIButton!
     
+    enum RecordingState {
+        case recording
+        case notRecording
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,43 +33,51 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         
         stopRecordingButton.isEnabled = false  // start with stopRecordingButton disabled
     }
- 
 
+    // called when the record button is pressed
     @IBAction func recordAudio(_ sender: Any) {
         
-        // disable record button and enable stop button while recording
-        stopRecordingButton.isEnabled = true
-        recordButton.isEnabled = false
-        recordingLabel.text = "Stop Recording"
+        // create an alert
+        let alertError = UIAlertController(title: "Warning!", message: "I'm sorry there was a problem trying to record your audio. Please try again", preferredStyle: .alert)
+        alertError.addAction(UIAlertAction(title: "Continue", style: .cancel, handler: nil))
+    
+        // update ui on button press
+        updateUI(.recording)
         
+        // record and save audio file
         let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
         let recordingName = "recordedVoice.wav"
         let pathArray = [dirPath, recordingName]
         let filePath = URL(string: pathArray.joined(separator: "/"))
-
         let session = AVAudioSession.sharedInstance()
-        try! session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
-
-        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        do {
+            try session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
+            try audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        } catch {
+            print("There was an error recording")
+            self.present(alertError, animated: true)
+        }
+        
         audioRecorder.delegate = self
         audioRecorder.isMeteringEnabled = true
         audioRecorder.prepareToRecord()
         audioRecorder.record()
     }
 
+    // called when the stop button is pressed
     @IBAction func stopRecording(_ sender: Any) {
         
-        // enable record button and disable stop button while recording
-        recordButton.isEnabled = true
-        stopRecordingButton.isEnabled = false
-        recordingLabel.text = "Tap to Record"
+        // update the ui
+        updateUI(.notRecording)
+        
+        // stop the recording
         audioRecorder.stop()
         let audioSession = AVAudioSession.sharedInstance()
         try! audioSession.setActive(false)
     }
     
+    // when the audio finishes recording
     func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
-        
         if flag {
             performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
         } else {
@@ -73,11 +85,27 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         }
     }
     
+    // send the data to the next view
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "stopRecording" {
             let playSoundsVC = segue.destination as! PlaySoundsViewController
             let recordedAudioUrl = sender as! URL
             playSoundsVC.recordedAudioURL = recordedAudioUrl
+        }
+    }
+    
+    // update UI
+    func updateUI(_ recordingState: RecordingState){
+        switch recordingState {
+            case .recording:
+                stopRecordingButton.isEnabled = true
+                recordButton.isEnabled = false
+                recordingLabel.text = "Stop Recording"
+                
+            case .notRecording:
+                recordButton.isEnabled = true
+                stopRecordingButton.isEnabled = false
+                recordingLabel.text = "Tap to Record"
         }
     }
 }
